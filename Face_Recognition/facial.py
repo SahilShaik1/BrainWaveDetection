0113d386c5223c29a2af3eb5198a0ddcfe0155b4
SahilShaik1 <89698739+SahilShaik1@users.noreply.github.com>
2023-11-18 18:41:24 -0700

    Update facial.py

--- Face_Recognition/facial.py
+++ Face_Recognition/facial.py
@@ -6,7 +6,6 @@ import os
 import time
 import tensorflow as tf
 from tensorflow import keras
-# define a video capture object
 
 
 ID = {
@@ -50,64 +49,65 @@ def control(path, id):
             end.append(temp.tolist())
     return end
 
+def search(src, id):
+    face_classifier = cv2.CascadeClassifier(
+        cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
+    )
+    path = creation(id)
+    counter = 0
+    limit_reached = False
+    while True and limit_reached is False:
+        ret, frame = src.read()
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = face_classifier.detectMultiScale(gray, 1.1, 4)
+        for (x, y, w, h) in faces:
+            ROI = frame[y: y + h, x: x + h]
+            ROI = cv2.resize(ROI, img_dims, cv2.INTER_LINEAR)
+            ROI = cv2.cvtColor(ROI, cv2.COLOR_BGR2GRAY)
+            ROI = sp_noise(ROI, .03)
+            if counter < limit:
+                cv2.imwrite(path + "\\" + str(counter) + ".jpg", ROI)
+                counter = counter + 1
+            else:
+                dims = frame.shape
+                limit_reached = True
+    return (path, dims)
+
+def rise(src):
+    face_classifier = cv2.CascadeClassifier(
+        cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
+    )
+    ret, frame = src.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    face = face_classifier.detectMultiScale(gray, 1.1, 4)
+    ROI = None
+    for (x, y, w, h) in face:
+        ROI = frame[y: y + h, x: x + h]
+        ROI = cv2.resize(ROI, img_dims, cv2.INTER_LINEAR)
+        ROI = cv2.cvtColor(ROI, cv2.COLOR_BGR2GRAY)
+    return ROI
 
 
-face_classifier = cv2.CascadeClassifier(
-    cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
-)
-
-dims = None
 img_dims = (28, 28)
 
 vid = cv2.VideoCapture(0)
-# Training period
-trainXA = np.array([])
-path = creation(0)
-counter = 0
-train_period = True
-while True and train_period is True:
-    ret, frame = vid.read()
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces = face_classifier.detectMultiScale(gray, 1.1, 4)
-    for (x, y, w, h) in faces:
-        ROI = frame[y: y+ h, x: x+ h]
-        ROI = cv2.resize(ROI, img_dims, cv2.INTER_LINEAR)
-        ROI = cv2.cvtColor(ROI,cv2.COLOR_BGR2GRAY)
-        ROI = sp_noise(ROI, .03)
-        if counter < limit:
-            cv2.imwrite(path + "\\" + str(counter) + ".jpg", ROI)
-            counter = counter + 1
-        else:
-            dims = frame.shape
-            train_period = False
+path, dims = search(vid, 0)
 
-path2 = creation(1)
-limit_reached = False
+
+
 print("Person A Finished.")
 time.sleep(3)
-counter = 0
-while True and limit_reached is False:
-    ret, frame = vid.read()
-    height = dims[0]
-    width = dims[1]
-    x = random.randint(0, width - img_dims[0])
-    y = random.randint(0, height - img_dims[1])
-    ROI = frame[y: y + img_dims[1], x: x + img_dims[0]]
-    ROI = cv2.cvtColor(ROI, cv2.COLOR_BGR2GRAY)
-    ROI = sp_noise(ROI, .03)
-    cv2.imwrite(path2 + "\\" + str(counter) + ".jpg", ROI)
-    counter = counter + 1
-    if counter >= limit:
-        limit_reached = True
 
-print("Entity B Finished")
+path2, _ = search(vid, 1)
+
+print("Person B Finished")
 
 # After the loop release    the cap object
 vid.release()
 # Destroy all the windows
 cv2.destroyAllWindows()
 
-
+# Training period
 training = control(path, 0)
 np.append(training, control(path2, 1))
 
@@ -116,10 +116,8 @@ training = np.array(training)
 #Data Recieved
 #Starting machine learning
 train_ratio = 0.95
-
 train_size = int(limit * train_ratio)
 dev_size = limit - train_size
-
 
 
 np.random.shuffle(training)
@@ -159,3 +157,8 @@ X_dev = X_dev.reshape(dev_size, img_dims[0], img_dims[1])
 
 model.evaluate(x=X_dev,y=Y_dev)
 
+print("Test:")
+time.sleep(5)
+face = rise(vid)
+cv2.imshow("face", face)
+print(model.predict(face))
