d64b5b6dbf0003ac27e5d076e11708d6558a463c
SahilShaik1 <89698739+SahilShaik1@users.noreply.github.com>
2023-11-24 15:09:53 -0700

    Update openbci.py

--- OpenBCI/openbci.py
+++ OpenBCI/openbci.py
@@ -1,34 +1,30 @@
 from pyOpenBCI import OpenBCICyton
 import numpy as np
 import pandas as pd
-
+from scipy import signal
+import time
 fs = 250.0
+EEG2UV_SCALE = (4500000)/24/(2**23-1)
+
+
+start = time.time()
 
 
 def print_freq(sample):
     #Add the samples that work
-    uVolts = (4500000)/24/(2**23-1) * sample
-    fft_vals = np.absolute(np.fft.rfft(uVolts))
-    fft_freq = np.fft.rfftfreq(len(sample), 1.0/fs)
-    eeg_bands = {
-        'Delta': (0, 4),
-        'Theta': (4, 8),
-        'Alpha': (8, 12),
-        'Beta': (12, 30),
-        'Gamma': (30, 45)
-    }
-    eeg_band_fft = dict()
-    for band in eeg_bands:
-        freq_ix = np.where((fft_freq >= eeg_bands[band][0]) & (fft_freq <= eeg_bands[band][1]))[0]
-        eeg_band_fft[band] = np.mean(fft_vals[freq_ix])
-    df = pd.DataFrame(columns=['band', 'val'])
-    df['band'] = eeg_bands.keys()
-    df['val'] = [eeg_band_fft[band] for band in eeg_bands]
-    ax = df.plot.bar(x='band', y='val', legend=False)
-    ax.set_xlabel("EEG band")
-    ax.set_ylabel("Mean band Amplitude")
+    sec = time.time() - start
+    if sec == 0:
+        sec = 1
+    print(sec)
+    sample = np.array(sample)
+    uVolts = EEG2UV_SCALE * sample
+    Volts = uVolts / 1000000
+    win = sec * fs
+    freq, psd = signal.welch(Volts, fs, nperseg = win)
+    print(freq)
+
+#/dev/ttyUSB1
 
 
-board = OpenBCICyton(port='COM4', daisy=True)
 
-board.start_stream(print_freq)
+print_freq([1, 2, 3, 4, 5, 6])
