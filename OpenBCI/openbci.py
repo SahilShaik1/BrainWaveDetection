c9a29d037d6ebf72f13029236f6cb4ac0a0f6cca
SahilShaik1 <89698739+SahilShaik1@users.noreply.github.com>
2023-11-24 13:21:44 -0700

    Update openbci.py

--- OpenBCI/openbci.py
+++ OpenBCI/openbci.py
@@ -1,20 +1,34 @@
 from pyOpenBCI import OpenBCICyton
 import numpy as np
+import pandas as pd
 
+fs = 250.0
 
 
 def print_freq(sample):
     #Add the samples that work
-    sample = [sample[0], ...]
     uVolts = (4500000)/24/(2**23-1) * sample
-    freq = abs(np.fft.fft(uVolts))
-    #Freq Set:
-    #Delta: < 3
-    #Theta: < 7
-    #Alpha: < 14
-    if freq < 7:
-        print("Drowsiness detected")
+    fft_vals = np.absolute(np.fft.rfft(uVolts))
+    fft_freq = np.fft.rfftfreq(len(sample), 1.0/fs)
+    eeg_bands = {
+        'Delta': (0, 4),
+        'Theta': (4, 8),
+        'Alpha': (8, 12),
+        'Beta': (12, 30),
+        'Gamma': (30, 45)
+    }
+    eeg_band_fft = dict()
+    for band in eeg_bands:
+        freq_ix = np.where((fft_freq >= eeg_bands[band][0]) & (fft_freq <= eeg_bands[band][1]))[0]
+        eeg_band_fft[band] = np.mean(fft_vals[freq_ix])
+    df = pd.DataFrame(columns=['band', 'val'])
+    df['band'] = eeg_bands.keys()
+    df['val'] = [eeg_band_fft[band] for band in eeg_bands]
+    ax = df.plot.bar(x='band', y='val', legend=False)
+    ax.set_xlabel("EEG band")
+    ax.set_ylabel("Mean band Amplitude")
 
-board = OpenBCICyton(port='COM4', daisy=False)
+
+board = OpenBCICyton(port='COM4', daisy=True)
 
 board.start_stream(print_freq)
